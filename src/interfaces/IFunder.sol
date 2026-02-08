3e17108fad068682609c73bf99610a5385696983
David Dada <daveproxy80@gmail.com>
2026-02-08 08:33:41 +0100

    Add interfaces

---
+++ src/interfaces/IFunder.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ICommon} from "./ICommon.sol";
+
+interface IFunderV4 {
+    /// @dev Should fund the account with the given transfers, after verifying the signature.
+    function fund(address account, bytes32 digest, ICommon.Transfer[] memory transfers, bytes memory funderSignature)
+        external;
+}
+
+interface IFunder is IFunderV4 {
+    /// @dev Checks if fund transfers are valid given a funderSignature.
+    /// @dev Funder implementations must revert if the signature is invalid.
+    function fund(bytes32 digest, ICommon.Transfer[] memory transfers, bytes memory funderSignature) external;
+}
